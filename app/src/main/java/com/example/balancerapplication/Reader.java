a2e4ab04af895e458bb0e9e5e5e2dc8a66bec444
shagushina <shagushina@bk.ru>
2019-04-05 15:49:35 +0300

    add class Reader

--- app/src/main/java/com/example/balancerapplication/Reader.java
+++ app/src/main/java/com/example/balancerapplication/Reader.java
@@ -1,4 +1,39 @@
 package com.example.balancerapplication;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class Reader {
+    public static JsonNode getUnitsNode() throws IOException{
+        //read json file data to String
+        byte[] jsonData = Files.readAllBytes(Paths.get("JSON_units_library.json"));
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonData);
+        return jsonNode;
+    }
+    public static JsonNode getModifiersNode() throws IOException{
+        //read json file data to String
+        byte[] jsonData = Files.readAllBytes(Paths.get("JSON_modifiers_library.json"));
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonData);
+        return jsonNode;
+    }
+    public static JsonNode getEnvironmentNode() throws IOException{
+        //read json file data to String
+        byte[] jsonData = Files.readAllBytes(Paths.get("JSON_environment_library.json"));
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonData);
+        return jsonNode;
+    }
+    //get name of entity
+    public String getStringKey(JsonNode jsonNode){
+        String str = null;
+        for (Iterator<String> it = jsonNode.fieldNames(); it.hasNext(); ) {
+            String key = it.next();
+            str = key;
+        }
+        return str;
+    }
 }
