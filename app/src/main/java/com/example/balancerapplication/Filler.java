d2a74d08006c02d919f5e4ebe76876dd6dab1671
Gradder <rzhitkov@gmail.com>
2019-04-12 22:32:37 +0300

    Fix conflicts

---
+++ app/src/main/java/com/example/balancerapplication/Filler.java
@@ -0,0 +1,139 @@
+package com.example.balancerapplication;
+
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Getter;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+
+public class Filler {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Getter
+    public class Entity {
+        private String name;
+        private Map<String, Integer> body;
+
+        Entity(){
+            this.name = null;
+            this.body = new TreeMap<>();
+        }
+
+        Entity(Entity u){
+            this.name = u.name;
+            for(Map.Entry<String, Integer> entry : u.body.entrySet()) {
+                this.body.put(entry.getKey(), entry.getValue());
+
+            }
+        }
+
+        Entity(String name, JsonNode node){
+            this.name = name;
+            Map<String, Integer> res = mapper.convertValue(node, Map.class);
+            this.body.putAll(res);
+        }
+
+    }
+
+    @Getter
+    public class Environment {
+        private String name;
+        private Map<String, Integer> body;
+
+        Environment(){
+            this.name = null;
+            this.body = new TreeMap<>();
+        }
+
+        Environment(Environment u){
+            this.name = u.name;
+            for(Map.Entry<String, Integer> entry : u.body.entrySet()) {
+                this.body.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        Environment(String name, JsonNode node){
+            this.name = name;
+            Map<String, Integer> res = mapper.convertValue(node, Map.class);
+            this.body.putAll(res);
+        }
+
+    }
+
+    @Getter
+    public class Modifier {
+        private String name;
+        private Map<String, Integer> body;
+
+        Modifier(){
+            this.name = null;
+            this.body = new TreeMap<>();
+        }
+
+        Modifier(Modifier u){
+            this.name = u.name;
+            for(Map.Entry<String, Integer> entry : u.body.entrySet()) {
+                this.body.put(entry.getKey(), entry.getValue());
+
+            }
+        }
+
+        Modifier(String name, JsonNode node){
+            this.name = name;
+            Map<String, Integer> res = mapper.convertValue(node, Map.class);
+            this.body.putAll(res);
+        }
+
+    }
+
+    private ArrayList<Entity> Entitys;
+    private ArrayList<Environment> Environments;
+    private ArrayList<Modifier> Modifiers;
+
+    Filler() {
+        this.Entitys = new ArrayList<>();
+        this.Environments = new ArrayList<>();
+        this.Modifiers = new ArrayList<>();
+    }
+
+    Filler(NodeStorage nodestorage) {
+        for (Map.Entry<String, JsonNode> entity : nodestorage.getEntitiesNodes().entrySet()) {
+            Entity e = new Entity(entity.getKey(), entity.getValue());
+            this.Entitys.add(e);
+        }
+
+        for (Map.Entry<String, JsonNode> modifier : nodestorage.getModificatorsNodes().entrySet()) {
+            Modifier m = new Modifier(modifier.getKey(), modifier.getValue());
+            this.Modifiers.add(m);
+        }
+
+        for (Map.Entry<String, JsonNode> environment : nodestorage.getEnvironmentsNodes().entrySet()) {
+            Environment en = new Environment(environment.getKey(), environment.getValue());
+            this.Environments.add(en);
+        }
+        
+
+    }
+
+    public ArrayList<Entity> getEntitiesObj() {
+        return Entitys;
+    }
+    public ArrayList<Environment> getEnvironmentObj() {
+        return Environments;
+    }
+    public ArrayList<Modifier> getModifiersObj() {
+        return Modifiers;
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/balancerapplication/NodeStorage.java
@@ -0,0 +1,75 @@
+
+package com.example.balancerapplication;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class NodeStorage {
+
+    public static final NodeStorage INSTANCE=new NodeStorage();
+
+
+    private final Map<String,JsonNode> entitiesStorage;
+    private final Map<String,JsonNode> modificatorsStorage;
+    private final Map<String,JsonNode> environmentsStorage;
+
+    private NodeStorage(){
+        this.entitiesStorage=new TreeMap<>();
+        this.modificatorsStorage=new TreeMap<>();
+        this.environmentsStorage=new TreeMap<>();
+    }
+
+//ENTITIES
+    public Map<String, JsonNode> getEntitiesNodes() {
+        return entitiesStorage;
+    }
+
+    public  JsonNode getEntityNodeByName(String name){
+        return this.entitiesStorage.get(name);
+    }
+
+    public void addEntityNode(String name,JsonNode node){
+        this.entitiesStorage.put(name,node);
+    }
+
+    public void addEntitiesNodes (Map<String,JsonNode> newNodes){
+        this.entitiesStorage.putAll(newNodes);
+    }
+
+    //MODIFICATORS
+    public Map<String, JsonNode> getModificatorsNodes() {
+        return modificatorsStorage;
+    }
+
+    public  JsonNode getModificatorNodeByName(String name){
+        return this.modificatorsStorage.get(name);
+    }
+
+    public void addModificatorNode(String name,JsonNode node){
+        this.modificatorsStorage.put(name,node);
+    }
+
+    public void addModificatorsNodes (Map<String,JsonNode> newNodes){
+        this.modificatorsStorage.putAll(newNodes);
+    }
+
+    //ENVIRONMENTS
+    public Map<String, JsonNode> getEnvironmentsNodes() {
+        return environmentsStorage;
+    }
+
+    public  JsonNode getEnvironmentNodeByName(String name){
+        return this.entitiesStorage.get(name);
+    }
+
+    public void addEnvironmentNode(String name,JsonNode node){
+        this.environmentsStorage.put(name,node);
+    }
+
+    public void addEnvironmentsNodes (Map<String,JsonNode> newNodes){
+        this.environmentsStorage.putAll(newNodes);
+    }
+
+}
